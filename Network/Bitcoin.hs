ec9b87d179e6743ff26ef5d617e16bce38c59d64
Michael Hendricks <michael@ndrix.org>
2011-08-05 11:16:03 -0600

    Slight cleanup

--- Network/Bitcoin.hs
+++ Network/Bitcoin.hs
@@ -7,30 +7,39 @@ import Network.Browser
 import Network.HTTP
 import Network.URI (URI,parseURI)
 
-jsonRPC cmd = "{\"jsonrpc\":\"2.0\",\"method\":\"" ++ cmd ++ "\", \"params\":[], \"id\":1}"
+jsonRPC cmd params = "{\"jsonrpc\":\"2.0\",\"method\":\"" ++ cmd ++ "\", \"params\":[], \"id\":1}"
+-- [ ("jsonrpc","2.0"),("method",cmd),("params",[]),("id":1) ]
 
 callBitcoinAPI :: String -> String -> String -> String -> [String] -> IO String
-callBitcoinAPI endpoint username password command params = do
+callBitcoinAPI urlString username password command params = do
     (_,res) <- browse $ do
         addAuthority authority
         setAllowBasicAuth True
         -- TODO "error" is null if no errors
         -- TODO "error" is hashref if errors (see "code" and "message")
         -- TODO "result" has data
-        request $ jsonReq $ jsonRPC command
+        request $ buildRequest urlString $ jsonRPC command params
     return $ rspBody res
-    where authority = AuthBasic {
-                        auRealm    = "jsonrpc",
-                        auUsername = username,
-                        auPassword = password,
-                        auSite     = serverURI
-                      }
-          serverURI = fromJust $ parseURI endpoint
-          jsonReq jsonBody = ( postRequest endpoint ){
-                rqBody = jsonBody,
-                rqHeaders = [
-                    mkHeader HdrContentType "application/json",
-                    mkHeader HdrContentLength (show $ length jsonBody)
-                ]
-            }
+    where authority = buildAuthority urlString username password
 
+
+
+-- Internal helper functions to make callBitcoinAPI more readable
+buildAuthority :: String -> String -> String -> Authority
+buildAuthority urlString username password =
+    AuthBasic {
+        auRealm    = "jsonrpc",
+        auUsername = username,
+        auPassword = password,
+        auSite     = uri
+    }
+    where uri = fromJust $ parseURI urlString
+buildRequest :: String -> String -> Request String
+buildRequest urlString jsonBody =
+    (postRequest urlString){
+        rqBody = jsonBody,
+        rqHeaders = [
+            mkHeader HdrContentType "application/json",
+            mkHeader HdrContentLength (show $ length jsonBody)
+        ]
+    }
